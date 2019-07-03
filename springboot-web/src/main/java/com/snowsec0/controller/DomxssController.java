6d8387d7bd4283074cb5e651cc314e14692d0e7a
shadow-horse <yuanjunhu30@gmail.com>
2019-07-04 00:39:27 +0800

    典型DOM XSS验证环境

--- springboot-web/src/main/java/com/snowsec0/controller/DomxssController.java
+++ springboot-web/src/main/java/com/snowsec0/controller/DomxssController.java
@@ -10,9 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DomxssController {
 	
 	@RequestMapping("/domxss")
-	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "world") String name,Model model) {
+	public String greeting(
+			@RequestParam(name = "name", required = false, defaultValue = "world") String name,
+			@RequestParam(name = "domxss1", required = false, defaultValue = "") String domxss1,
+			@RequestParam(name = "domxss2", required = false, defaultValue = "") String domxss2,
+			@RequestParam(name = "domxss3", required = false, defaultValue = "") String domxss3,
+			@RequestParam(name = "domxss4", required = false, defaultValue = "") String domxss4,
+			Model model) {
         model.addAttribute("xname", name);
+        model.addAttribute("domxss1", domxss1);
+        model.addAttribute("domxss2", domxss2);
+        model.addAttribute("domxss3", domxss3);
+        model.addAttribute("domxss4", domxss4);
+        System.out.println(domxss1);
         return "index";
     }
-
+	
 }
