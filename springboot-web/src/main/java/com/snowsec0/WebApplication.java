9ee30db2cb3aceb259409734c92552bbbaa33c07
shadow-horse <yuanjunhu30@gmail.com>
2019-07-08 00:22:24 +0800

    未解决

--- springboot-web/src/main/java/com/snowsec0/WebApplication.java
+++ springboot-web/src/main/java/com/snowsec0/WebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.util.UrlPathHelper;
 
 @SpringBootApplication
-public class WebApplication extends WebMvcConfigurerAdapter{
+public class WebApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
@@ -15,10 +15,12 @@ public class WebApplication extends WebMvcConfigurerAdapter{
 	
 	//springboot中URL带有斜杠的转义字符百分之2F导致的400错误
 	//https://blog.csdn.net/u011419453/article/details/25736375 未解决
+	/*
 	@Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         UrlPathHelper urlPathHelper = new UrlPathHelper();
         urlPathHelper.setUrlDecode(false);
         configurer.setUrlPathHelper(urlPathHelper);
     }
+    */
 }
