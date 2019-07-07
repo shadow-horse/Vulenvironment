b29a9ec97cb42a16e41497d268ee6e40e0d0c4bf
shadow-horse <yuanjunhu30@gmail.com>
2019-07-08 00:19:42 +0800

    解决spring boot参数存在特殊字符报错的问题

--- springboot-web/src/main/java/com/snowsec0/WebApplication.java
+++ springboot-web/src/main/java/com/snowsec0/WebApplication.java
@@ -2,12 +2,22 @@ package com.snowsec0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-
+	
+	//springboot中URL带有斜杠的转义字符百分之2F导致的400错误
+	@Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setUrlDecode(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
 }
