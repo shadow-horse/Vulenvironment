c0daf32b3a907e7025a8d72c1bc610c257b4db5e
shadow-horse <yuanjunhu30@gmail.com>
2019-07-08 00:45:53 +0800

    解决redirect跳转报错，返回值写错误

--- springboot-web/src/main/java/com/snowsec0/controller/DomxssController.java
+++ springboot-web/src/main/java/com/snowsec0/controller/DomxssController.java
@@ -1,5 +1,11 @@
 package com.snowsec0.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +17,42 @@ public class DomxssController {
 	
 	@RequestMapping("/domxss")
 	public String greeting(
+			HttpServletRequest request,
 			@RequestParam(name = "name", required = false, defaultValue = "world") String name,
 			@RequestParam(name = "domxss1", required = false, defaultValue = "") String domxss1,
 			@RequestParam(name = "domxss2", required = false, defaultValue = "") String domxss2,
 			@RequestParam(name = "domxss3", required = false, defaultValue = "") String domxss3,
 			@RequestParam(name = "domxss4", required = false, defaultValue = "") String domxss4,
+			@RequestParam(name = "redirect", required = false, defaultValue = "") String redirect,
 			Model model) {
+		
+		String urls = request.getRequestURL().toString();
+		
+//		try {
+//			redirect = URLDecoder.decode(redirect, "utf-8");
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			redirect = "";
+//			e.printStackTrace();
+//		}
+		
+		if(redirect.indexOf(":") != -1)
+		{
+			
+		}else
+		{
+			redirect = urls.substring(0,urls.lastIndexOf("/")) + "/" +redirect;
+		}
+		
+		
         model.addAttribute("xname", name);
         model.addAttribute("domxss1", domxss1);
         model.addAttribute("domxss2", domxss2);
         model.addAttribute("domxss3", domxss3);
         model.addAttribute("domxss4", domxss4);
+        model.addAttribute("redirect", redirect);
+        
+        
         System.out.println(domxss1);
         return "index";
     }
