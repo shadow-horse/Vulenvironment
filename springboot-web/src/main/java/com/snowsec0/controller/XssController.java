9589750cf44d5d5e668c65966b3fc721c7c5c511
shadow-horse <yuanjunhu30@gmail.com>
2019-07-08 00:46:28 +0800

    条件信息获取接口

--- springboot-web/src/main/java/com/snowsec0/controller/XssController.java
+++ springboot-web/src/main/java/com/snowsec0/controller/XssController.java
@@ -1,5 +1,7 @@
 package com.snowsec0.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +16,24 @@ public class XssController {
 		System.out.println("welcome "+user);
 		return "welcome "+user;
 	}
-
+	
+	@RequestMapping("update")
+	public String update(@RequestParam(name = "domxss4", required = false, defaultValue = "") String domxss4)
+	{
+		return domxss4;
+	}
+	
+	@RequestMapping("redirect")
+	public String redirect(HttpServletRequest request,@RequestParam(name = "redirect", required = false, defaultValue = "") String redirect)
+	{
+		String urls = request.getRequestURL().toString();
+		if(redirect.indexOf(":") != -1)
+		{
+			return redirect;
+		}
+		
+		urls = urls.substring(0,urls.lastIndexOf("/")) + "/" +redirect;
+		
+		return urls;
+	}
 }
